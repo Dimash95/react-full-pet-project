a83e8b5586113a502a61011beb87efa6d3bd75ca
Dimash <dinmukhamed.amirov@gmail.com>
2024-03-27 20:15:00 +0500

    feat: add switch theme

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,10 +2,20 @@ import { FaReact } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
 import styles from "./footer.module.css";
+import classNames from "classnames";
+import { ThemeContext } from "../../context";
+import { useContext } from "react";
 
 const Footer = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div className={styles.wrapper}>
+    <div
+      className={classNames(
+        theme === "dark" ? styles.wrapperDark : styles.wrapperLight,
+        styles.wrapper
+      )}
+    >
       <FaReact className={styles.logo} />
       <div className={styles.uni}>Made by Dimash</div>
       <div className={styles.githubLinkedin}>
