dfbddbff2ac8dc3edf504d81907ec1b5cfc818e8
Dimash <dinmukhamed.amirov@gmail.com>
2024-03-31 17:33:50 +0500

    feat: add active link

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { FaReact } from "react-icons/fa";
 import { TiShoppingCart } from "react-icons/ti";
 import { Switch } from "antd";
@@ -9,6 +9,9 @@ import classNames from "classnames";
 
 const Header = () => {
   const { theme, setTheme } = useContext(ThemeContext);
+
+  const setActivePage = ({ isActive }: { isActive: boolean }) =>
+    isActive ? styles.activeLink : styles.link;
 
   const onChange = (checked: boolean) => {
     setTheme(checked ? "dark" : "light");
@@ -25,30 +28,30 @@ const Header = () => {
       <FaReact className={styles.logo} />
       <ul className={styles.list}>
         <li>
-          <Link to="/react-full-pet-project/" className={styles.link}>
+          <NavLink to="/react-full-pet-project/" className={setActivePage}>
             Home
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/react-full-pet-project/catalog/1" className={styles.link}>
+          <NavLink to="/react-full-pet-project/catalog/1" className={setActivePage}>
             Catalog
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/react-full-pet-project/about" className={styles.link}>
+          <NavLink to="/react-full-pet-project/about" className={setActivePage}>
             About
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/react-full-pet-project/contacts" className={styles.link}>
+          <NavLink to="/react-full-pet-project/contacts" className={setActivePage}>
             Contacts
-          </Link>
+          </NavLink>
         </li>
       </ul>
       <div className={styles.icons}>
-        <Link to="/react-full-pet-project/cart" className={styles.link}>
+        <NavLink to="/react-full-pet-project/cart" className={setActivePage}>
           <TiShoppingCart className={styles.cart} />
-        </Link>
+        </NavLink>
 
         <Switch defaultChecked={theme === "dark" ? true : false} onChange={onChange} />
       </div>
--- src/pages/catalog/catalog-categories/catalog-categories.tsx
+++ src/pages/catalog/catalog-categories/catalog-categories.tsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import { useState } from "react";
 import CatalogCard from "./catalog-card/catalog-card";
 import { Input } from "antd";
@@ -20,19 +20,22 @@ const Category = ({ categories }: CategoryProps) => {
     setSearchedValue(value);
   }
 
+  const setActiveCategory = ({ isActive }: { isActive: boolean }) =>
+    isActive ? styles.activeLink : styles.link;
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.categoriesSortSearch}>
         <div className={styles.categories}>
           {categories.map((category) => (
-            <Link
-              className={styles.name}
+            <NavLink
+              className={setActiveCategory}
               to={`/react-full-pet-project/catalog/${category.id}`}
               key={category.id}
               onClick={() => setSearchedValue("")}
             >
               {category.name}
-            </Link>
+            </NavLink>
           ))}
         </div>
         <div className={styles.sortSearch}>
