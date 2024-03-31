62989416481c6cab6fdcf939babc27dd6b151513
Dimash <dinmukhamed.amirov@gmail.com>
2024-04-01 01:31:40 +0500

    refactor: refactor catalog

--- src/pages/catalog/catalog.tsx
+++ src/pages/catalog/catalog.tsx
@@ -1,10 +1,32 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { NavLink, useParams } from "react-router-dom";
+
 import { getCategories } from "../../api/get-categories";
-import Category from "./catalog-categories/catalog-categories";
+import CatalogCard from "./catalog-card/catalog-card";
+import { CategoryType } from "../../types/category";
+
+import { Input } from "antd";
 import styles from "./catalog.module.css";
 
 function Catalog() {
-  const [categories, setCategories] = useState([]);
+  const { categoryId = "" } = useParams();
+  const inputRef = useRef(null);
+  const [searchedValue, setSearchedValue] = useState(localStorage.getItem("searchedValue") || "");
+
+  const [categories, setCategories] = useState<CategoryType[]>([]);
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearchedValue(e.target.value);
+    localStorage.setItem("searchedValue", e.target.value);
+  }
+
+  function clearSearch() {
+    setSearchedValue("");
+    localStorage.setItem("searchedValue", "");
+  }
+
+  const setActiveCategory = ({ isActive }: { isActive: boolean }) =>
+    isActive ? styles.activeLink : styles.link;
 
   const displayCategories = async () => {
     const fetchedItems = await getCategories();
@@ -15,10 +37,40 @@ function Catalog() {
 
   useEffect(() => {
     displayCategories();
-  }, []);
+    if (inputRef.current) (inputRef.current as HTMLInputElement)?.focus();
+  }, [searchedValue]);
+
   return (
-    <div className={styles.content}>
-      <Category categories={categories} />
+    <div className={styles.wrapper}>
+      <div className={styles.categoriesSortSearch}>
+        <div className={styles.categories}>
+          {categories.map((category) => (
+            <NavLink
+              className={setActiveCategory}
+              to={`/react-full-pet-project/catalog/${category.id}`}
+              key={category.id}
+              onClick={() => setSearchedValue("")}
+            >
+              {category.name}
+            </NavLink>
+          ))}
+        </div>
+        <div className={styles.sortSearch}>
+          <Input
+            ref={inputRef}
+            placeholder="Search ..."
+            style={{ width: 300 }}
+            value={searchedValue}
+            onChange={handleChange}
+          />
+          <p className={styles.clear} onClick={clearSearch}>
+            Clear
+          </p>
+        </div>
+      </div>
+      <div>
+        <CatalogCard categoryId={categoryId} searchedValue={searchedValue} />
+      </div>
     </div>
   );
 }
